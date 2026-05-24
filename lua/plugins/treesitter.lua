748c2382cfb673bdc4b41bdf8fa124cf3152d153
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2026-05-24 14:11:09 -0600

    2026-05-24 14:11

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-
     opts = {
       ensure_installed = { "lua", "python", "vim", "bash" },
       highlight = { enable = true },
